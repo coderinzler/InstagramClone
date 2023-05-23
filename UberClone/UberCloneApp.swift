63f78decd879d595647660050e4d90c251da201f
Lorenzo Navarro <lorenzo.navaarro@gmail.com>
2023-05-23 01:51:15 -0700

    Initial Commit

---
+++ UberClone/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by user239884 on 5/23/23.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
